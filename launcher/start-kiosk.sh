d292a7ef2a194751170a38138096fff317aa6749
helipilot19792-blip <helipilot19792@gmail.com>
2026-07-07 15:27:54 -0400

    Launch device service from kiosk session

--- launcher/start-kiosk.sh
+++ launcher/start-kiosk.sh
@@ -14,15 +14,4 @@ openbox-session &
 
 sleep 2
 
-# Launch the Stay Compass PWA.
-# This will eventually be replaced by the Stay Compass Device Service.
-
-exec chromium \
-  --kiosk \
-  --app=https://stayinniagara.com/compass/ \
-  --noerrdialogs \
-  --disable-infobars \
-  --disable-session-crashed-bubble \
-  --disable-features=TranslateUI \
-  --autoplay-policy=no-user-gesture-required \
-  --check-for-update-interval=31536000
+exec python3 /opt/stay-compass/device/stay-compass-device.py
